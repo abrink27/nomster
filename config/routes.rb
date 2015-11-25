a4b11c5b201029fe487dc641f9f32cb645e33b39
Andrew Brink <abrink27@gmail.com>
2015-11-25 06:56:37 +0000

    controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Nomster::Application.routes.draw do
+
+  root 'places#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,7 +11,7 @@ Nomster::Application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #   get 'products/:isd/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
