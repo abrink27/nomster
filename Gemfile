5a772e84b9b3d926a2d1b1f238f726c4f31d671e
Andrew Brink <abrink27@gmail.com>
2015-12-09 03:03:36 +0000

    BING API added

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem "figaro", ">=1.0.0"
+
 gem 'bootstrap-sass', '~> 3.3.5.1'
 
 gem 'sprockets', '2.11.0'
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,8 +2,8 @@ class Place < ActiveRecord::Base
 	belongs_to :user
 	geocoded_by :address
   after_validation :geocode
-	
-	validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 50}, :minimum => 3}
+
+	validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 50, :minimum => 3}
 	validates :address, :presence => true, :uniqueness => true
 	validates :description, :presence => true, :length => { :maximum => 1000, :minimum => 15}
 end
